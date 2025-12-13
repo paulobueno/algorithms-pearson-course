30fd90b1963bbafaefe498ca07873774e1454093
Paulo Bueno <paulob.bruno@gmail.com>
2025-12-13 17:21:48 -0300

    Adding download files in test

--- algorithms/src/test/java/com/algorithms/unionfind/QuickFindUFTest.java
+++ algorithms/src/test/java/com/algorithms/unionfind/QuickFindUFTest.java
@@ -1,23 +1,48 @@
 package com.algorithms.unionfind;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 class QuickFindUFTest {
-  private Path testFile;
+  private final static String FILES_DIRECTORY = "src/test/resources";
+  private final static String BASE_URL = "https://algs4.cs.princeton.edu/15uf/";
+  private final static List<String> fileNames = List.of("tinyUF.txt", "mediumUF.txt", "largeUF.txt");
 
-  @BeforeEach
-  void setUp() throws Exception {
-    String remoteUrl = "https://algs4.cs.princeton.edu/15uf/tinyUF.txt";
-    testFile = Paths.get("tinyUF.txt");
+  @BeforeAll
+  static void downloadFiles() {
+    for (String fileName : fileNames) {
+      Path filePath = Paths.get(FILES_DIRECTORY).resolve(fileName);
+      if (!Files.exists(filePath)) {
+        try {
+          URL url = new URL(BASE_URL + fileName);
+          InputStream in = url.openStream();
+          Files.copy(in, filePath);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
   }
 
   @Test
-  public tinyTest() {
-    var myImp = new QuickFindUF(10);
-    var officialImp = new edu.princeton.cs.algs4.QuickFindUF(10);
+  void dummyTest() {
+    System.out.println("HERE IS MY TEST");
   }
+
+//  @ParameterizedTest
+//  @ValueSource(strings = {"tinyuf.txt"});
+//  void mainTest(String fileName) {
+//  }
 }
